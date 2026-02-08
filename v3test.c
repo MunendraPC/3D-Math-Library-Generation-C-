8178c692ba4dd34ac212dbae7e29a8cc74929d91
Moon <mp2893@nau.edu>
2026-02-07 18:17:35 -0700

    Add print statements for vector operation results
    Added printf statements to display results of vector operations.

--- v3test.c
+++ v3test.c
@@ -6,6 +6,8 @@ void tes_v3_add()
 	
 	v3_add(r, w, v);
 	// compare output for what's expected
+
+    printf("v3_add result: (%f, %f, %f)\n", r[0], r[1], r[2]);
 	
 }
 
@@ -17,6 +19,8 @@ void tes_v3_sub()
 	
 	v3_subtract(r, w, v);
 	// compare output for what's expected
+
+    printf("v3_subtract result: (%f, %f, %f)\n", r[0], r[1], r[2]);
 	
 }
 
@@ -25,6 +29,21 @@ void tes_v3_dotProd()
     float v[] = {1.0, 0.0, 0.0};
     float w[] = {1.0, 0.0, 0.0};
     float result = v3_dot_product(v, w);
+
     // compare result for what's expected
+    printf("v3_dot_product result: (%f)\n", result);
+
+}
+
+void tes_v3_scale()
+{
+    float v[] = {1.0, 0.0, 0.0};
+    float scaler = 2.0;
+
+    v3_scale(v, scaler);
+
+    // compare output for what's expected
+    printf("v3_scale output: (%f, %f, %f)\n", v[0], v[1], v[2]);
+
 
 }
