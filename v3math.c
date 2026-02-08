55755c9f15e0fec9cd2f859192a74c560df3b4be
Moon <mp2893@nau.edu>
2026-02-07 19:05:47 -0700

    Refactor v3math.c for formatting and braces
    Removed unnecessary blank lines and added missing braces.

--- v3math.c
+++ v3math.c
@@ -5,8 +5,6 @@ void v3_add(float *dst, float *a, float *b)
 	dst[0] = a[0] + b[0];
 	dst[1] = a[1] + b[1];
 	dst[2] = a[2] + b[2];
-	
-	
 }
 
 // form v3 from a to b
@@ -32,8 +30,9 @@ float v3_dot_product(float *a, float *b){
 	return (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2]);
 }
 void v3_cross_product(float *dst, float *a, float *b){
-	
-	
+	dst[0] = a[1]*b[2] - a[2]*b[1];
+	dst[1] = a[2]*b[0] - a[0]*b[2];
+	dst[2] = a[0]*b[1] - a[1]*b[0];
 }
 void v3_scale(float *dst, float s){
 
@@ -51,16 +50,26 @@ float v3_angle(float *a, float *b){
 float v3_angle_quick(float *a, float *b){
 	return v3_dot_product(a, b)/ (sqrt((a[0]*a[0]) + a[1]*a[1] + a[2]*a[2]) * sqrt(b[0]*b[0] + b[1]*b[1] + b[2]*b[2]));
 	
-} 
+}
+// reflect v across n
 void v3_reflect(float *dst, float *v, float *n){
-	
-	
+	float dot = v3_dot_product(v, n);
+	float scaled_n[3] = {n[0] * 2 * dot, n[1] * 2 * dot, n[2] * 2 * dot};
+	dst[0] = v[0] - scaled_n[0];
+	dst[1] = v[1] - scaled_n[1];
+	dst[2] = v[2] - scaled_n[2];
 }
+// length of a
 float v3_length(float *a){
-	
+	return sqrt(a[0]*a[0] + a[1]*a[1] + a[2]*a[2]);
 	
 }
+// normalizing the a variable
 void v3_normalize(float *dst, float *a){
-	
-	
+	float length = ve_length(a);
+	if(length > 0) {
+		dst[0] = a[0] / length;
+		dst[1] = a[1] / length;
+		dst[2] = a[2] / length;
+	}
 }
